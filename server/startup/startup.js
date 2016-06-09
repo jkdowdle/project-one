d5f5ac98fad1805d8f2f627f627edec7d9f3658a
Josh D <jkdowdle@gmail.com>
2016-06-09 12:52:58 -0600

    Improved project structure

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -3,11 +3,43 @@ Meteor.startup(() => {
 	process.env.MAIL_URL = "smtp://postmaster%40sandbox234048e4d3b647c6bfb9615d3a952819.mailgun.org:97420af15ba02c1416320bc1c1d0dea7@smtp.mailgun.org:587";
 
 	if(Meteor.users.find().count() < 1) {
-		var id = Accounts.createUser({
+		// Create Admin
+		let admin = Accounts.createUser({
 		  	email: 'admin@flueint.com',
 		  	password: 'admin',
-		  	profile: {name: 'Administrator'}
+		  	profile: {name: 'Administrator'}		  	
 		});
-		Roles.addUsersToRoles(id, 'admin');
+		Roles.addUsersToRoles(admin, 'admin');
+		Meteor.users.update(admin, {$set: {"emails.0.verified": true} });
+
+		// Create Teacher
+		let teacherName = 'Techer Man';
+		let rosterId = TeachersRosters.insert({ students: [ {dummyData: 'arrayLengthOne'} ] });
+		let teacher = Accounts.createUser({
+			email: 'teacher@flueint.com',
+		  	password: 'teacher',
+		  	profile: {
+		  		name: teacherName,
+		  		rosterId: rosterId
+		    }
+		});
+		TeachersRosters.update(rosterId, {$set: { teacherId: teacher } });
+		Roles.addUsersToRoles(teacher, 'teacher');
+		Meteor.users.update(teacher, {$set: {"emails.0.verified": true} });
+
+		// Create Student
+		let student = Accounts.createUser({
+			email: 'student@gmail.com',
+		  	password: 'student',
+		  	profile: {
+		  		name: 'Student Boy',
+		  		teacher: teacherName,
+		  		teachersRosterId: rosterId
+		  	}
+		});
+		Roles.addUsersToRoles(student, 'student');
+		Meteor.users.update(student, {$set: {"emails.0.verified": true} });
 	}
 });
+
+		
