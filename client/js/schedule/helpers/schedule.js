f4486b281de9d838e5a1d867afe579d21293e2ea
Josh D <jkdowdle@gmail.com>
2017-01-23 17:14:01 -0700

    Improved scheduling system

--- client/js/schedule/helpers/schedule.js
+++ client/js/schedule/helpers/schedule.js
@@ -5,6 +5,144 @@ Template.schedule.helpers({
 
 		if (creditAmount < 5){
 			return 'credit-warning';
-		}	
-	}
+		}
+	},
+/*	nextSession() {
+		let userId = Meteor.userId(),
+		//		now = moment().toISOString(),
+				nextSession;
+
+		if ( Roles.userIsInRole(userId, 'teacher') ) {
+			let rosterId = Accounts && Accounts.users.findOne(userId).profile.rosterId;
+
+			var now = new Date().getTime();
+	    var msInDay = 1000 * 60 * 60 * 24;
+
+	      nextSession = Events.find(
+					{
+	        	timeStart: {
+	          	'$gte': ISODate("2017-01-20T23:00:00Z")
+		        }
+		      },
+					{
+						sort: { 'timeStart': 1 }
+					},
+					{
+						'limit': 1
+					}
+				);
+
+			//nextSession = Events.findOne({ 'teachersRosterId': rosterId, timeStart: { '$gte': new Date().toISOString() } }, { 'sort': { 'timeStart': 1 } }, { 'limit': 1});
+
+									//	db.coll.find({name: 'montalto', 'users.username': 'ciccio'}).count() new ISODate("2012-01-12T20:15:31Z")
+    } else {
+			nextSession = Events.findOne({ 'scheduledStudent': userId }, { 'sort': { 'timeStart': 1 } });
+		}
+
+		console.log(moment().toISOString());
+
+		return moment(nextSession.timeStart).format('YYYY-MM-DD hh:mm');
+	}, */
+	selectedDayEvents() {
+		let selectedDay = Session.get('selectedDay'),
+				daysAppts   = Events.find({ 'start': selectedDay }, { 'sort': { 'timeStart': 1 } }),
+				existAppts;
+
+		if (selectedDay) {
+			selectedDay = moment(selectedDay).format('MM/DD/YYYY');
+		}
+
+		if ( daysAppts.count() > 0 ) {
+			existAppts = true;
+		} else {
+			existAppts = false;
+		}
+
+		return {
+			appts: daysAppts,
+			day: selectedDay,
+			apptsExist: existAppts
+		};
+	},
+	selectedDay() {
+		let selectedDay = Session.get('selectedDay');
+
+		if (selectedDay) {
+			selectedDay = moment(selectedDay).format('MM/DD/YYYY');
+		}
+
+		return selectedDay;
+	},
+	getMorning() {
+		let currentUser = Meteor.userId(),
+				timezone = Accounts.users.findOne(currentUser).profile.timezone.name,
+		 		selectedDay    = Session.get('selectedDay'),
+				morningStarts  = moment(selectedDay).tz(timezone).startOf('day').hour(0).minute(0).toISOString(),
+				morningEnds    = moment(selectedDay).tz(timezone).startOf('day').hour(12).minute(0).toISOString(),
+				available      = false;
+
+		morningStarts = new Date(morningStarts);
+		morningEnds   = new Date(morningEnds);
+
+		let	morningAppts = Events.find({ 'timeStart': { '$gte': morningStarts, '$lt': morningEnds } }, { 'sort': { 'timeStart': 1 } });
+
+		if ( morningAppts && morningAppts.count() > 0 ) {
+			available = true;
+		}
+
+		return {
+			appointments:	morningAppts,
+			available: available
+		}
+	},
+	getAfternoon() {
+		let currentUser = Meteor.userId(),
+				timezone = Accounts.users.findOne(currentUser).profile.timezone.name,
+		 		selectedDay    = Session.get('selectedDay'),
+				afternoonStarts  = moment(selectedDay).tz(timezone).startOf('day').hour(12).minute(0).toISOString(),
+				afternoonEnds    = moment(selectedDay).tz(timezone).startOf('day').hour(17).minute(0).toISOString(),
+				available      = false;
+
+		afternoonStarts = new Date(afternoonStarts);
+		afternoonEnds   = new Date(afternoonEnds);
+
+		let	afternoonAppts = Events.find({ 'timeStart': { '$gte': afternoonStarts, '$lt': afternoonEnds } }, { 'sort': { 'timeStart': 1 } });
+
+		if ( afternoonAppts && afternoonAppts.count() > 0 ) {
+			available = true;
+		}
+
+		return {
+			appointments:	afternoonAppts,
+			available: available
+		}
+	},
+	getEvening() {
+		let currentUser = Meteor.userId(),
+				timezone = Accounts.users.findOne(currentUser).profile.timezone.name,
+		 		selectedDay    = Session.get('selectedDay'),
+				eveningStarts  = moment(selectedDay).tz(timezone).startOf('day').hour(17).minute(0).toISOString(),
+				eveningEnds    = moment(selectedDay).tz(timezone).startOf('day').hour(24).minute(0).toISOString(),
+				available      = false;
+
+		console.log(eveningStarts);
+		console.log(eveningEnds);
+
+		eveningStarts = new Date(eveningStarts);
+		eveningEnds   = new Date(eveningEnds);
+
+		// console.log(eveningStarts);
+		// console.log(eveningEnds);
+
+		let	eveningAppts = Events.find({ /*"start": '2017-01-24'*/ 'timeStart': { '$gte': eveningStarts, '$lt': eveningEnds } }, { 'sort': { 'timeStart': 1 } });
+
+		if ( eveningAppts && eveningAppts.count() > 0 ) {
+			available = true;
+		}
+
+		return {
+			appointments:	eveningAppts,
+			available: available
+		}
+	},
 });
--- client/js/schedule/schedule.js
+++ client/js/schedule/schedule.js
@@ -1,20 +1,5 @@
 // Schedule Branch
 
-$( '#events-calendar' ).fullCalendar( 'changeView', 'basicWeek' );
-
-$( '#events-calendar' ).css( 'color', 'pink' );
-
-Template.schedule.rendered = function() {
-/*
-  $( '#events-calendar' ).fullCalendar({
-    header: {
-     left: 'prev,next today',
-     center: 'title',
-     right: 'month,agendaWeek,agendaDay'
-    },
-  });*/
-};
-
 let isPast = ( date ) => {
     let today = moment().format();
     return moment( today ).isAfter( date );
@@ -24,77 +9,251 @@ let beforeAppointment = '';
 let apptPerDay = 1;
 let day = 1;
 
+Template.schedule.rendered = function() {
+	let today = moment().format('YYYY-MM-DD');
+
+  $(`[data-date='${today}']`).addClass('fc-today');
+
+  $('[data-date="2017-01-21"]').css('background', 'pink');
+};
+
 Template.schedule.onCreated( () => {
 
     let template = Template.instance();
     template.subscribe( 'events' );
+
+    Session.set('selectedDay', null);
+});
+
+Template.schedule.events({
+  'click .fc-month-button' () {
+    Session.set('selectedDay', null);
+  },
+  'click .add-appt' () {
+    let currentUser = Meteor.userId();
+    if ( Roles.userIsInRole(currentUser, 'teacher') ) {
+      Session.set( 'eventModal', { type: 'add', date: Session.get('selectedDay') } );
+      $( '#add-edit-event-modal' ).modal( 'show' );
+    }
+  },
+  'click .fc-basicWeek-button' () {
+    $('.fc-state-highlight').removeClass('fc-state-highlight');
+    let today = moment().format('YYYY-MM-DD');
+    $(`[data-date='${today}']`).addClass('fc-today fc-state-highlight');
+  },
+  'click .appointment' () {
+    Session.set( 'eventModal', { type: 'edit', event: this._id } );
+    $( '#add-edit-event-modal' ).modal( 'show' );
+  },
+  'click .fc-prev-button' () {
+    $('.fc-state-highlight').removeClass('fc-state-highlight');
+    let today = moment().format('YYYY-MM-DD');
+    $(`[data-date='${today}']`).addClass('fc-today');
+  },
+  'click .fc-next-button' () {
+    $('.fc-state-highlight').removeClass('fc-state-highlight');
+    let today = moment().format('YYYY-MM-DD');
+    $(`[data-date='${today}']`).addClass('fc-today');
+  }
 });
 
 Template.schedule.onRendered( () => {
+
     $( '#events-calendar' ).fullCalendar({
+      firstDay: 1,
+      height: 600,
+      header: {
+       left: 'title',
+       center: '',
+       right: 'month,basicWeek,today,prev,next'
+      },
 
+      events( start, end, timezone, callback ) {
 
+          let currentUser = Meteor.userId();
 
-        eventRender( event, element ) {
+          if(Roles.userIsInRole(currentUser, 'teacher')){
+              teachersRosterId = Accounts.users.findOne(currentUser).profile.rosterId;
+              let timezone = Accounts.users.findOne(currentUser).profile.timezone.name;
+              let data = Events.find({teachersRosterId: teachersRosterId}, {sort: {timeStart: 1}}).fetch().map( ( event ) => {
+                  event.editable = !isPast( event.start );
+                  return {
+                      _id: event._id,
+                      title: event.title,
+                      start: event.timeStart, // moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
+                      end: event.timeEnd, // moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
+                      timeStart: event.timeStart,
+                      timeEnd: event.timeEnd,
+                      status: event.status,
+                      teachersRosterId: event.teachersRosterId,
+                      scheduledStudent: event.scheduledStudent
+                  }
+              });
 
-          if (event.status === 'Filled'){
-            element.find('.fc-content').toggleClass('filled');
+              if ( data ) {
+                  callback( data );
+              }
+
+          } else if (Roles.userIsInRole(currentUser, 'student')) {
+              teachersRosterId = Accounts.users.findOne(currentUser).profile.teachersRosterId;
+              let timezone = Accounts.users.findOne(currentUser).profile.timezone.name;
+              let data = Events.find({/*teachersRosterId: teachersRosterId,*/ scheduledStudent: { $in: ["Not Yet Available", currentUser ] } }, {sort: {timeStart: 1}}).fetch().map( ( event ) => {
+                  return {
+                      _id: event._id,
+                      title: event.title,
+                      start:  event.timeStart, //moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
+                      end:  event.timeStart, //moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
+                      timeStart: event.timeEnd,
+                      timeEnd: event.timeEnd,
+                      status: event.status,
+                      teachersRosterId: event.teachersRosterId,
+                      scheduledStudent: event.scheduledStudent
+                  }
+              });
+
+              if ( data ) {
+                  callback( data );
+              }
           }
 
-          let currentUser = Meteor.userId(),
-              usersTimezone = Accounts.users.findOne(currentUser).profile.timezone.name,
-              time = moment(event.timeStart);
+      },
 
-          element.find( '.fc-content' )
-            .html(`
-              <div class="row">
-                <div class="col-xs-12">
-                  <h5>${ time.tz(usersTimezone).format('LT') }</h5>
-                </div>
+      eventRender( event, element, view ) {
+
+        if ( view.type === 'month' || view.type === 'basicWeek' ) {
+          $(element).css("display", "none");
+        }
+
+        $(element).each(function () {
+          $(this).attr('date-num', event.start.format('YYYY-MM-DD'));
+        });
+
+        //if ( view.name === 'month' ) {
+        //  return false;
+        //}
+
+        // if (event.status === 'Filled') {
+        //   element.find('.fc-content').toggleClass('filled');
+        // }
+
+        let currentUser = Meteor.userId(),
+            usersTimezone = Accounts.users.findOne(currentUser).profile.timezone.name,
+            time = moment(event.timeStart);
+
+        element.find( '.fc-content' )
+          .html(`
+            <div class="row">
+              <div class="col-xs-12">
+                <h5>${ time.tz(usersTimezone).format('LT') }</h5>
               </div>
-            `
-          );
-        },
+            </div>
+          `
+        );
+      },
 
-        eventDrop( event, delta, revert ) {
-            let date = event.start.format();
-            if ( !isPast( date ) ) {
+      eventAfterAllRender(view, x, y, z) {
+        let currentUser = Meteor.userId();
 
-                let update = {
-                    _id: event._id,
-                    start: date,
-                    end: date
-                };
+        if ( view.type === 'basicWeek' ) {
+          $('#events-calendar').fullCalendar('option', 'height', 199);
+        } else {
+          $('#events-calendar').fullCalendar('option', 'height', 600);
+        }
 
-                Meteor.call( 'editEvent', update, ( error ) => {
-                    if ( error ) {
-                        Bert.alert( error.reason, 'danger' );
-                    }
-                });
+        $('.appointment-count').remove();
+        for ( cDay = view.start.clone(); cDay.isBefore(view.end) ; cDay.add(1, 'day') ) {
+          let dateNum = cDay.format('YYYY-MM-DD'),
+              dayEl = $('.fc-day[data-date="' + dateNum + '"]'),
+              totalNum = Events && Events.find({ "start": dateNum }).count(),
+              filledNum = Events && Events.find({ "start": dateNum, "status": "Filled" }).count(),
+              openNum = Events && Events.find({ "start": dateNum, "status": "Open" }).count();
+
+          if( totalNum > 0 /* && view.name === 'month' */) {
+            let pluralApt = 'Appts',
+                eventCountTemplate;
+
+            if( Roles.userIsInRole(currentUser, 'teacher') ) {
+
+              if ( openNum === 1 ) {
+                pluralApt = 'Appt';
+              }
+
+              eventCountTemplate = `
+                <div class="appointment-count text-center" style="margin-top: 2.5rem">
+                  <i>${ filledNum }/${ totalNum }</i> <span class="hidden-xs hidden-sm"> - Filled ${ pluralApt }</span>
+                </div>
+              `;
             } else {
-                revert();
-                Bert.alert( 'Sorry, you can\'t move items to the past!', 'danger' );
+
+              if ( totalNum === 1 ) {
+                pluralApt = 'Appt';
+              }
+
+              eventCountTemplate = `
+                <div class="appointment-count text-center" style="margin-top: 2.5rem">
+                  <i>${ openNum }</i> <span class="hidden-xs hidden-sm"> - Open ${ pluralApt }</span>
+                </div>
+              `;
             }
-        },
-        dayClick( date ) {
-            let currentUser = Meteor.userId();
 
-            if(Roles.userIsInRole(currentUser, 'teacher')){
-                Session.set( 'eventModal', { type: 'add', date: date.format() } );
-                $( '#add-edit-event-modal' ).modal( 'show' );
-            }
 
-            $( '#events-calendar' ).fullCalendar( 'changeView', 'basicWeek' );
+            dayEl.append(eventCountTemplate);
 
-            getEvents();
-            renderEvents();
-        },
-        eventClick( event, jsEvent, view ) {
-            console.log( jsEvent, view );
+          }
+        }
+      },
 
-            Session.set( 'eventModal', { type: 'edit', event: event._id } );
+      eventDrop( event, delta, revert ) {
+          let date = event.start.format();
+          if ( !isPast( date ) ) {
+
+              let update = {
+                  _id: event._id,
+                  start: date,
+                  end: date
+              };
+
+              Meteor.call( 'editEvent', update, ( error ) => {
+                  if ( error ) {
+                      Bert.alert( error.reason, 'danger' );
+                  }
+              });
+          } else {
+              revert();
+              Bert.alert( 'Sorry, you can\'t move items to the past!', 'danger' );
+          }
+      },
+      dayClick( date, jsEvent, view ) {
+        let currentUser = Meteor.userId();
+        let today = moment().format('YYYY-MM-DD');
+
+        if ( view.name === 'month' ) {
+          $( '#events-calendar' ).fullCalendar( 'changeView', 'basicWeek' );
+          $( '#events-calendar' ).fullCalendar('gotoDate', date);
+
+        } else {
+          $( '#events-calendar' ).fullCalendar('gotoDate', date);
+        }
+
+        if ( Roles.userIsInRole(currentUser, 'teacher') && date.format() === Session.get('selectedDay') ) {
+            Session.set( 'eventModal', { type: 'add', date: date.format() } );
             $( '#add-edit-event-modal' ).modal( 'show' );
         }
+
+        //ad fc-today to th
+        $(`th[data-date='${today}']`).addClass('fc-today');
+        // reset selected day
+        $(".fc-state-highlight").removeClass("fc-state-highlight");
+        //highlight day th & td
+        $(`[data-date='${date.format('YYYY-MM-DD')}']`).addClass('fc-state-highlight');
+
+        Session.set('selectedDay', date.format() );
+      },
+      eventClick( event, jsEvent, view ) {
+
+          Session.set( 'eventModal', { type: 'edit', event: event._id } );
+          $( '#add-edit-event-modal' ).modal( 'show' );
+      }
     });
 
     Tracker.autorun( () => {
@@ -102,86 +261,3 @@ Template.schedule.onRendered( () => {
         $( '#events-calendar' ).fullCalendar( 'refetchEvents' );
     });
 });
-
-function renderEvents() {
-  $( '#events-calendar' ).fullCalendar({
-    eventRender( event, element, view ) {
-    console.log('rendering events');
-
-      if (event.status === 'Filled'){
-        element.find('.fc-content').toggleClass('filled');
-      }
-
-      let currentUser = Meteor.userId(),
-          usersTimezone = Accounts.users.findOne(currentUser).profile.timezone.name,
-          time = moment(event.timeStart);
-
-      element.find( '.fc-content' )
-        .html(`
-          <div class="row">
-            <div class="col-xs-12">
-              <h5>${ time.tz(usersTimezone).format('LT') }</h5>
-            </div>
-          </div>
-        `
-      );
-    },
-  });
-  eventRender();
-};
-
-function getEvents() {
-  console.clear();
-  console.log('getting events');
-
-  $('#events-calendar').fullCalendar({
-    events( start, end, timezone, callback ) {
-
-        let currentUser = Meteor.userId();
-
-        if(Roles.userIsInRole(currentUser, 'teacher')){
-            teachersRosterId = Accounts.users.findOne(currentUser).profile.rosterId;
-            let timezone = Accounts.users.findOne(currentUser).profile.timezone.name;
-            let data = Events.find({teachersRosterId: teachersRosterId}, {sort: {timeStart: 1}}).fetch().map( ( event ) => {
-                event.editable = !isPast( event.start );
-                return {
-                    _id: event._id,
-                    title: event.title,
-                    start: moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
-                    end: moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
-                    timeStart: event.timeStart,
-                    timeEnd: event.timeEnd,
-                    status: event.status,
-                    teachersRosterId: event.teachersRosterId,
-                    scheduledStudent: event.scheduledStudent
-                }
-            });
-
-            if ( data ) {
-                callback( data );
-            }
-        } else if (Roles.userIsInRole(currentUser, 'student')) {
-            teachersRosterId = Accounts.users.findOne(currentUser).profile.teachersRosterId;
-            let timezone = Accounts.users.findOne(currentUser).profile.timezone.name;
-            let data = Events.find({/*teachersRosterId: teachersRosterId,*/ scheduledStudent: { $in: ["Not Yet Available", currentUser ] } }, {sort: {timeStart: 1}}).fetch().map( ( event ) => {
-                return {
-                    _id: event._id,
-                    title: event.title,
-                    start: moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
-                    end: moment(event.timeStart).tz(timezone).format('YYYY-MM-DD'),
-                    timeStart: event.timeStart,
-                    timeEnd: event.timeEnd,
-                    status: event.status,
-                    teachersRosterId: event.teachersRosterId,
-                    scheduledStudent: event.scheduledStudent
-                }
-            });
-
-            if ( data ) {
-                callback( data );
-            }
-        }
-    },
-  });
-  events();
-}
